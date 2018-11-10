fa0f48964334fc7f2e1f42248841565dd46ef3f3
Yasar Kücükkaya <yasar@yk-code.de>
2018-11-11 00:43:56 +0100

    Fixed port

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
