42ffecb55b00c7816d70bb516250a65e6920231d
Yasar Kücükkaya <yasar@yk-code.de>
2018-11-11 00:17:51 +0100

    Added docs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   root(): string {
-    return 'Hello World!';
+    return `
+      <h1><a href="/">Youtube MP3/RSS API</a></h1>
+      <div>
+        <h3>API: </h3>
+        <div>/channel/:id/rss.xml</div>
+        <div>v/:id/file.mp3</div>
+      </div>
+      <div>
+        <h3>Examples: </h3>
+      </div>
+      <ul>
+        <li><a href="/channel/UCgj0uCW9VR8p3PUJ91oDz9g/rss.xml">RSS for channel: /channel/UCgj0uCW9VR8p3PUJ91oDz9g/rss.xml</a></li>
+        <li><a href="/v/K6Huy-xn7zw/file.mp3">MP3 for video: /v/K6Huy-xn7zw/file.mp3</a></li>
+      </ul>
+    `;
   }
 }
